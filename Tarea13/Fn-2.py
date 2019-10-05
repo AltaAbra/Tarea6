b248fc4d7981703441d148643539c0571e02a9e6
AltaAbra <altaabra_itneverends@hotmail.com>
2019-10-04 22:43:27 -0500

    Examenes

---
+++ Tarea13/Fn-2.py
@@ -0,0 +1,12 @@
+'''
+Version Fn-2
+Crear una funcion que retorne el enesimo numero non
+'''
+
+def nNon(num):
+    return num*2-1
+
+print(nNon(1))#1
+print(nNon(3))#5
+print(nNon(100))#199
+print(nNon(1298734))#2597467
